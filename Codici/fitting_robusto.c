0a1fe818a18cdd19927bb9319cf10e82888b093f
Godente <lorenzo.godente@gmail.com>
2025-03-09 12:17:48 +0100

    secondo commit

--- Codici/fitting_robusto.c
+++ Codici/fitting_robusto.c
@@ -15,11 +15,11 @@ void stampa_matrice(double *matrix, size_t righe, size_t colonne)
     printf("\n");
 }
 
-void trasposta (double *matrix1, size_t righe1, size_t colonne1, double *holder)
+void trasposta(double *matrix1, size_t righe1, size_t colonne1, double *holder)
 {
-    for(size_t i = 0; i < colonne1; i++)
+    for (size_t i = 0; i < colonne1; i++)
     {
-        for(size_t j = 0; j < righe1; j++)
+        for (size_t j = 0; j < righe1; j++)
         {
             holder[i * righe1 + j] = matrix1[j * colonne1 + i];
         }
@@ -53,7 +53,6 @@ void enhanced_matrix(double *mat, size_t righe, size_t col, double *enhanced_mat
         {
             enhanced_mat[i * 2 * col + j] = mat[i * col + j]; // Parte sinistra: matrice originale
             enhanced_mat[i * 2 * col + col + j] = (i == j) ? 1.0 : 0.0; // Parte destra: matrice identità
-                                              //condizione ? valore_se_vero : valore_se_falso
         }
     }
 }
@@ -78,6 +77,13 @@ int gauss_inversion(double *mat, size_t riga, size_t col, double *i_mat)
 {
     double *e_mat = (double *)malloc(riga * 2 * col * sizeof(double));
     double *row_copy = (double *)malloc(2 * col * sizeof(double));
+
+    if (!e_mat || !row_copy)
+    {
+        free(e_mat);
+        free(row_copy);
+        return 0; // Errore di allocazione
+    }
 
     enhanced_matrix(mat, riga, col, e_mat);
 
@@ -128,7 +134,7 @@ int gauss_inversion(double *mat, size_t riga, size_t col, double *i_mat)
             i_mat[i * col + j] = e_mat[i * 2 * col + col + j];
         }
     }
-    //quando si lavora con i malloc bisogna ricordarsi di liberare la memoria
+
     free(e_mat);
     free(row_copy);
     return 1; // Successo
@@ -140,42 +146,62 @@ void prod_matrice(double *matrice1, size_t righe1, size_t colonne1, double *matr
     {
         for (size_t j = 0; j < colonne2; j++)
         {
-            holder[ i * colonne2 + j] = 0;
+            holder[i * colonne2 + j] = 0;
             for (size_t k = 0; k < colonne1; k++)
             {
                 holder[i * colonne2 + j] += matrice1[i * colonne1 + k] * matrice2[k * colonne2 + j];
             }
-
         }
     }
 }
 
-void matrice_vandermon (double *mat, size_t righe, size_t col, double *val)
+void matrice_vandermon(double *mat, size_t righe, size_t col, double *val)
 {
-    for(size_t i = 0; i < righe; i++)
+    for (size_t i = 0; i < righe; i++)
     {
-        for(size_t j = 0; j < col; j++)
+        for (size_t j = 0; j < col; j++)
         {
             mat[i * col + j] = pow(val[i], j);
         }
     }
 }
 
-void coefficent (double *x, size_t row, size_t col, double *y, double *risultato)
+int coefficent(double *x, size_t row, size_t col, double *y, double *risultato)
 {
-    double *vander_mat = (double*)malloc(row * col * sizeof(double));
-    double *holder = (double*)malloc(row * col * sizeof(double));
-    double *vander_trasposta = (double*)malloc(row * col * sizeof(double));
-    double *denominatore = (double*)malloc(col * col * sizeof(double));
-    double *numeratore = (double*)malloc(col * sizeof(double));
-    
+    double *vander_mat = (double *)malloc(row * col * sizeof(double));
+    double *holder = (double *)malloc(row * col * sizeof(double));
+    double *vander_trasposta = (double *)malloc(row * col * sizeof(double));
+    double *denominatore = (double *)malloc(col * col * sizeof(double));
+    double *numeratore = (double *)malloc(col * sizeof(double));
+
+    if (!vander_mat || !holder || !vander_trasposta || !denominatore || !numeratore)
+    {
+        free(vander_mat);
+        free(holder);
+        free(vander_trasposta);
+        free(denominatore);
+        free(numeratore);
+        return 0; // Errore di allocazione
+    }
+
     matrice_vandermon(vander_mat, row, col, x);
     trasposta(vander_mat, row, col, vander_trasposta);
     prod_matrice(vander_trasposta, col, row, vander_mat, col, holder);
-    gauss_inversion(holder, col, col, denominatore);
+
+    if (!gauss_inversion(holder, col, col, denominatore))
+    {
+        free(vander_mat);
+        free(holder);
+        free(vander_trasposta);
+        free(denominatore);
+        free(numeratore);
+        return 0; // Errore nell'inversione
+    }
+
     prod_matrice(vander_trasposta, col, row, y, 1, numeratore);
     prod_matrice(denominatore, col, col, numeratore, 1, risultato);
-    // "Debug"
+
+    // Debug
     printf("vander matrix\n");
     stampa_matrice(vander_mat, row, col);
     printf("vander trasposta\n");
@@ -192,23 +218,36 @@ void coefficent (double *x, size_t row, size_t col, double *y, double *risultato
     free(vander_trasposta);
     free(denominatore);
     free(numeratore);
+    return 1; // Successo
 }
 
 int main()
 {
- double x[5] = {1, 2, 3, 4, 5};
- double y[5] = {1.5, 2, 1.5, 0, -2.5};
+    double x[5] = {1, 2, 3, 4, 5};
+    double y[5] = {1.5, 2, 1.5, 0, -2.5};
 
- size_t ordine = 4;
- size_t dim_x = sizeof(x) / sizeof(x[0]);
- size_t dim_y = sizeof(y) / sizeof(y[0]);
+    size_t ordine = 4;
+    size_t dim_x = sizeof(x) / sizeof(x[0]);
+    size_t dim_y = sizeof(y) / sizeof(y[0]);
 
- double *coefficenti = (double*)malloc(ordine * sizeof(double));
+    double *coefficenti = (double *)malloc(ordine * sizeof(double));
 
- coefficent(x, dim_x, ordine, y, coefficenti);
- stampa_matrice(coefficenti, ordine, 1);
+    if (!coefficenti)
+    {
+        fprintf(stderr, "Errore di allocazione della memoria.\n");
+        return 1;
+    }
 
- free(coefficenti);
+    if (!coefficent(x, dim_x, ordine, y, coefficenti))
+    {
+        fprintf(stderr, "Errore nel calcolo dei coefficienti.\n");
+        free(coefficenti);
+        return 1;
+    }
 
-return 0;
+    printf("Coefficienti del polinomio:\n");
+    stampa_matrice(coefficenti, ordine, 1);
+
+    free(coefficenti);
+    return 0;
 }
